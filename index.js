6d82ba7bfa8afb37b398761059cf7f63ffa3f9df
jmalave15 <qmalavejose1@gmail.com>
2023-06-10 13:02:45 -0400

    sql problem and index.js working

--- index.js
+++ index.js
@@ -9,8 +9,158 @@ const db = mysql.createConnection(
         // MySQL username,
         user: 'root',
         // MySQL password
-        password: 'password',
+        password: '',
         database: 'employee_tracker',
     },
     console.log(`Connected to the employee_tracker database.`)
 );
+
+function showDepartments() {
+    db.query("SELECT * FROM department", function (err, result) {
+      if (err) {
+        console.log("ERROR:");
+        console.log(err);
+      }
+      console.table(result);
+      chooseFunction();
+    });
+  }
+  
+  function showRoles() {
+    db.query(
+      "SELECT role.id, title, salary, department.name as department FROM role JOIN department ON role.department_id = department.id;",
+      function (err, result) {
+        if (err) {
+          console.log("ERROR:");
+          console.log(err);
+        }
+        console.table(result);
+        chooseFunction();
+      }
+    );
+  }
+  
+  function showEmployees() {
+    db.query(
+      'SELECT \
+      employee.id, \
+      employee.first_name, \
+      employee.last_name, \
+      role.title, \
+      department.name as department, \
+      role.salary, \
+      CONCAT(manager.first_name, " ", manager.last_name) as manager \
+      FROM employee \
+      JOIN role ON employee.role_id = role.id \
+      JOIN department ON role.department_id = department.id \
+      LEFT OUTER JOIN employee manager ON employee.manager_id = manager.id;',
+      function (err, result) {
+        if (err) {
+          console.log("ERROR:");
+          console.log(err);
+        }
+        console.table(result);
+        chooseFunction();
+      }
+    );
+  }
+  
+  async function addDepartment() {
+    let dep = await inquirer.prompt([
+      {
+        type: "input",
+        message: "Name of the department?",
+        name: "dep",
+      },
+    ]);
+  
+    db.query(
+      `INSERT INTO department (name) VALUES (?)`,
+      dep.dep,
+      (err, result) => {
+        if (err) {
+          console.log("ERROR:");
+          console.log(err);
+        }
+        chooseFunction();
+      }
+    );
+  }
+  
+  async function addRole() {
+    db.query("SELECT id, name FROM department", async function (err, result) {
+      if (err) {
+        console.log("ERROR:");
+        console.log(err);
+      }
+      let userResponse = await inquirer.prompt([
+        {
+          type: "input",
+          message: "What is the name of the role?",
+          name: "role",
+        },
+        {
+          type: "number",
+          message: "What is your roles salary?",
+          name: "salary",
+        },
+        {
+          type: "list",
+          message: "Choose a department for your role: ",
+          name: "dep",
+          choices: result,
+        },
+      ]);
+  
+      let depId = -1;
+      for (i = 0; i < result.length; i++) {
+        if (result[i].name == userResponse.dep) {
+          depId = result[i].id;
+        }
+      }
+  
+      db.query(
+        "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);",
+        [userResponse.role, userResponse.salary, depId],
+        (err, result) => {
+          if (err) {
+            console.log("ERROR:");
+            console.log(err);
+          }
+          chooseFunction();
+        }
+      );
+    });
+  }
+  
+  async function chooseFunction() {
+    let ans = await inquirer.prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "func",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update an Employee Role",
+        ],
+      },
+    ]);
+  
+    if (ans.func == "View All Departments") {
+      showDepartments();
+    } else if (ans.func == "View All Roles") {
+      showRoles();
+    } else if (ans.func == "View All Employees") {
+      showEmployees();
+    } else if (ans.func == "Add a Department") {
+      addDepartment();
+    } else if (ans.func == "Add a Role") {
+      addRole();
+    }
+  }
+  chooseFunction();
--- seeds.sql
+++ seeds.sql
@@ -6,39 +6,40 @@ USE employee_tracker;
 
 CREATE TABLE department (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(40),
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(40),
+    title VARCHAR(30),
     salary DECIMAL,
     department_id INT
 );
 
 CREATE TABLE employee (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(40),
-    last_name VARCHAR(40),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
     manager_id INT
 );
 
-INSERT INTO department (title) VALUES
-    ('Sales'),
-    ('Development'),
-    ('Accounting'),
-    ('Marketing');
+INSERT INTO department (name) 
+VALUES
+    ( 1, "Sales"),
+    ( 2, "Development"),
+    ( 3, "Accounting"),
+    ( 4, "Marketing");
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-    ('Salesperson', 100000, 1),
-    ('Sales Manager', 80000, 1),
-    ('Junior Developer', 150000, 2),
-    ('Senior Developer', 120000, 2),
-    ('Accountant', 125000, 3),
-    ('Account Manager', 160000, 3),
-    ('Marketing Manager', 250000, 4);
+    ("Salesperson", 100000, 1),
+    ("Sales Manager", 80000, 1),
+    ("Junior Developer", 150000, 2),
+    ("Senior Developer", 120000, 2),
+    ("Accountant", 125000, 3),
+    ("Account Manager", 160000, 3),
+    ("Marketing Manager", 250000, 4);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
@@ -46,7 +47,7 @@ VALUES
     ( 2, "Jose", "Jose", 2, 1),
     ( 3, "Julio", "Rodriguez", 3, NULL),
     ( 4, "Kevin", "Fulton", 4, 3),
-    ( 5, "Kunal", "Robins", 5, NULL),
+    ( 5, "Kuna", "Robins", 5, NULL),
     ( 6, "Sean", "Brown", 6, 5),
     ( 7, "Natalie", "Lord", 7, NULL),
     ( 8, "Tom", "Allen", 8, 7);
